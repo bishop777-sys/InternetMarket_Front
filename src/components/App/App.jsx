91f2dacd213e250aea267a3253e078bae187ccd0
bishop777-sys <bishop_777@rambler.ru>
2020-05-02 15:14:22 +0200

    Add react router and homePage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,11 @@
 import React, {Fragment, Component } from 'react'
-
+import {Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Header from '../Header/Header.jsx'
 import ErrorBoundry from '../error-boundry/error-boundry.jsx'
 import './App.css'
 import ProductList from '../ProductList/ProductList.jsx';
 import ResponsiveDrawer from '../MainPanel/MainPanel.jsx';
+import HomePage from '../HomePage/HomePage.jsx';
 import IconBreadcrumbs from '../BreadCrumbs/BreadCrumbs.jsx';
 import { Container } from '@material-ui/core';
 
@@ -24,7 +25,14 @@ class App extends Component {
                     <Header />
                     <Container>
                         <ResponsiveDrawer>
-                                    <ProductList />
+                            <Router>
+                                <Switch>
+                                    <Route exact path="/" component={HomePage} />
+                                    <Route exact path="/product" component={ProductList} />
+                                </Switch>
+                                
+
+                            </Router>
                         </ResponsiveDrawer>
                     </Container>
                 </Fragment>
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default class HomePage extends React.PureComponent{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>HomePage</div>
+        )
+    }
+}
